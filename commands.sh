6714d72f8585df91a9588cecfa55a4462a18d192
Colby Ford <colbytylerford@gmail.com>
2026-05-04 13:33:31 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -8,3 +8,4 @@ quarto render --to html
 quarto render --to asciidoc
 
 # Then add :leveloffset: +1 to the beginning of the generated .adoc file to adjust heading levels.
+# Also add [role="pagenumrestart"] to the beginning of Chapter 1 to reset page numbering for the PDF export.
